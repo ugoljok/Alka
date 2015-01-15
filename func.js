73911738d3f1f24d902c9448e1c3e424de665001
Elena <ugoljok91@gmail.com>
2015-01-15 22:31:23 +0400

    правка багов, реализация выигрыша и алгоритм последней клетки

--- func.js
+++ func.js
@@ -72,17 +72,21 @@ function currentPlayer(result){
 	var position_first = getPlayerByIndex(obj_pl,obj_mv.id).position; // получаем текущую позицию текущего игрока
 	var cell_index = position_first+result-1; // индекс задания	
 	// Дошли до финиша
-	if(cell_index+1>arrCell.length){
+	if( (cell_index+1>arrCell.length && position_first!=arrCell.length) || (result==1 && position_first==arrCell.length) ){
 		//alert("Ты победил!");
+	victory(obj_pl, obj_mv);
+	}
+	else if(result>1 && position_first==arrCell.length){
 		document.getElementById("photo").src = getPlayerByIndex(obj_pl,obj_mv.id).photo; //выводим фото
 		document.getElementById("player_name").innerHTML = getPlayerByIndex(obj_pl,obj_mv.id).name+" "+getPlayerByIndex(obj_pl,obj_mv.id).surname; //выводим имя
 		document.getElementById("player_position").innerHTML = ""; //выводим позицию в окно
-		document.getElementById("player_kubik").innerHTML = "Тебе выпало число: "+result; //выводим выпавшее число в окно 
-		document.getElementById("player_fant").innerHTML = "Поздравляю! Ты добрался до финиша! Можешь расслабиться и отдохнуть;)";
+		document.getElementById("player_kubik").innerHTML = "Тебе выпало число: "+result; //выводим выпавшее число в окно
+		document.getElementById("player_fant").innerHTML = "Неудачник! Топай на старт!)))"; //выводим выпавший фант в окно
+		speak(getPlayerByIndex(obj_pl,obj_mv.id).name+", тебе не повезло...Да что там не повезло! Дуй на старт, днище ебаное!"); // говорилка
 		document.getElementById("color-header_player").style.background=getPlayerByIndex(obj_pl,obj_mv.id).color;
-		// вывод картинки из победителя
-		document.getElementById("form_currentPlayer").style.background = '#FFFFFF url(images/win.jpg) 0% 100% no-repeat';
-	}else{
+	// вывод картинки из ячейки
+		document.getElementById("form_currentPlayer").style.background = '#FFFFFF url(images/fail.jpg) 0% 100% no-repeat';
+		}else{
 	// выводим инфу на форму
 	document.getElementById("photo").src = getPlayerByIndex(obj_pl,obj_mv.id).photo; //выводим фото
 	document.getElementById("player_name").innerHTML = getPlayerByIndex(obj_pl,obj_mv.id).name+" "+getPlayerByIndex(obj_pl,obj_mv.id).surname; //выводим имя
@@ -103,15 +107,19 @@ function playersMove(){
 	obj_mv = JSON.parse(ls.get("move")); // получаем id текущего игрока
 	var position_first = getPlayerByIndex(obj_pl,obj_mv.id).position; // получаем текущую позицию текущего игрока
 	var cell_index = position_first+result-1; // индекс задания
-
-
-	
-	if((cell_index+1) > parseInt(arrCell.length)){
-		console.log("Позиция игрока", (cell_index+1), " выходит за пределы игрового поля. Удаляем игрока");
-		// Удалит игрока и запишит в лс обьект без текщего игрока
-		ls.set ("players",JSON.stringify( delPlayerByIndex(players,obj_mv.id) )); 
+	var position_last;
+	if((cell_index+1) >= parseInt(arrCell.length)){
+		if(result>1 && position_first==arrCell.length){
+			position_last=1;
+			}else{
+				// у нас есть победитель!
+				// Удалит игрока и запишит в лс обьект без текщего игрока
+				console.log("Позиция игрока", (cell_index+1), " выходит за пределы игрового поля. Удаляем игрока");
+				ls.set ("players",JSON.stringify( delPlayerByIndex(players,obj_mv.id) )); 
+				return false;
+			};
+		
 	}else{
-		var position_last;
 		// если указано на сколько клеток надо идти вперед\назад
 		if(arrCell[cell_index].step){
 			position_last = position_first+parseInt(arrCell[cell_index].step);
@@ -121,23 +129,10 @@ function playersMove(){
 			if(parseInt(arrCell[cell_index].rel)==22){position_last = 22;}
 			if(parseInt(arrCell[cell_index].rel)==60){position_last = 60;}
 			if(arrCell[cell_index].rel=="pass"){position_last = position_first;}
-			if(arrCell[cell_index].rel=="kamikadze"){
-				if(parseInt(result)>1){
-					position_last=1;
-					alert ("Неудачник! Топай на старт!)))")
-				}else{
-					// у нас есть победитель!
-					alert("Поздравляю! Ты добрался до финиша! Можешь расслабиться и отдохнуть;)");
-					position_last=position_first;
-					// Удалит игрока и запишит в лс обьект без текщего игрока
-					ls.set ("players",JSON.stringify( delPlayerByIndex(players,obj_mv.id) )); 
-					obj_pl = JSON.parse(ls.get("players")); // получаем игроков
-				};
-			};
 		}else{
 			position_last = position_first+result; // получаем новую позицию текущего игрока
 		};
-	
+	};
 		getPlayerByIndex(obj_pl,obj_mv.id).position = position_last; // присваиваем новое значение
 		ls.set ("players",JSON.stringify(obj_pl)); // записываем новые данные в лс
 		
@@ -148,7 +143,7 @@ function playersMove(){
 			obj_mv.id=0;
 		};
 		ls.set ("move",JSON.stringify(obj_mv)); // записываем новые данные в лс
-	};
+	
 	
 	console.log("Игрок", obj_mv.id, "    Кубик:",result,"   Позиция:", position_first, " -> ", position_last || (cell_index+1));
 
@@ -202,3 +197,15 @@ for(var k in obj){
 if(i===index){delete obj[k]; return obj; };
 i++;
 }};
+
+function victory(obj_pl, obj_mv){
+		document.getElementById("photo").src = getPlayerByIndex(obj_pl,obj_mv.id).photo; //выводим фото
+		document.getElementById("player_name").innerHTML = getPlayerByIndex(obj_pl,obj_mv.id).name+" "+getPlayerByIndex(obj_pl,obj_mv.id).surname; //выводим имя
+		document.getElementById("player_position").innerHTML = ""; //выводим позицию в окно
+		document.getElementById("player_kubik").innerHTML = "Тебе выпало число: "+result; //выводим выпавшее число в окно 
+		document.getElementById("player_fant").innerHTML = "Поздравляю! Ты добрался до финиша! Можешь расслабиться и отдохнуть;)";
+		document.getElementById("color-header_player").style.background=getPlayerByIndex(obj_pl,obj_mv.id).color;
+		speak(getPlayerByIndex(obj_pl,obj_mv.id).name+", твою мать. Я так счастлива! Иди слей масло. Только не долго, а то подумают что какаешь."); // говорилк
+		// вывод картинки из победителя
+		document.getElementById("form_currentPlayer").style.background = '#FFFFFF url(images/win.jpg) 0% 100% no-repeat';
+	}
